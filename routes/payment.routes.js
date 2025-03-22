f873ce1d909e6d41dc90363f6be4780ac9d2d47e
ritshubh <shubhsharmass242424@gmail.com>
2025-03-22 16:10:55 +0530

    Update payment.routes.js

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -7,6 +7,6 @@ const {
 } = require("../controllers/payment.controller");
 
 router.post("/create-order", createOrder);
-router.post("/status", veriFyPayment);
+router.post("/status/:txnId", veriFyPayment);
 
 module.exports = router;
