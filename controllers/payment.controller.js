286e37cec66b91732f1616f3c6aaa880e9e1b6ee
ritshubh <shubhsharmass242424@gmail.com>
2025-03-22 16:03:38 +0530

    Update payment.controller.js

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -21,63 +21,68 @@ const MERCHANT_STATUS_URL =
 const MERCHANT_STATUS_URL_PROD =
 	"https://api.phonepe.com/apis/hermes/pg/v1/status";
 
-// const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
-// const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/status"
-
-const redirectUrl = "https://priykala-backend.vercel.app/status";
+const redirectUrl = "http://localhost:4000/api/status";
 
 const successUrl = "http://localhost:4000/payment-success";
 const failureUrl = "http://localhost:4000/payment-failure";
 
 exports.createOrder = async (req, res) => {
-	const { name, mobile, amount, transactionId, MUID } = req.body;
-	console.log("TT", req.body);
-
-	//payment
-	const paymentPayload = {
-		merchantId: MERCHANT_ID,
-		merchantUserId: name,
-		mobileNumber: mobile,
-		amount: amount * 100,
-		merchantTransactionId: transactionId,
-		redirectUrl: `${redirectUrl}/?id=${transactionId}`,
-		redirectMode: "POST",
-		paymentInstrument: {
-			type: "PAY_PAGE",
-		},
-	};
-
-	const payload = Buffer.from(JSON.stringify(paymentPayload)).toString(
-		"base64"
-	);
-	const keyIndex = 1;
-	const string = payload + "/pg/v1/pay" + MERCHANT_KEY;
-	const sha256 = crypto.createHash("sha256").update(string).digest("hex");
-	const checksum = sha256 + "###" + keyIndex;
-
-	const option = {
-		method: "POST",
-		url: MERCHANT_BASE_URL_PROD,
-		headers: {
-			accept: "application/json",
-			"Content-Type": "application/json",
-			"X-VERIFY": checksum,
-		},
-		data: {
-			request: payload,
-		},
-	};
 	try {
-		const response = await axios.request(option);
-		console.log(
-			"ORD",
-			response.data.data.instrumentResponse.redirectInfo.url
-		);
-		res.status(200).json({
-			msg: "OK",
-			url: response.data.data.instrumentResponse.redirectInfo.url,
-			resp: response.data,
-		});
+		const { name, mobile, amount, transactionId, MUID } = req.body;
+		console.log("TT", req.body);
+
+		//payment
+		const paymentPayload = {
+			merchantId: MERCHANT_ID,
+			merchantTransactionId: transactionId,
+			merchantUserId: MUID,
+			name: name,
+			mobileNumber: mobile,
+			amount: amount * 100,
+			redirectUrl: `${redirectUrl}/${transactionId}`,
+			redirectMode: "POST",
+			paymentInstrument: {
+				type: "PAY_PAGE",
+			},
+		};
+
+		const payloadMain = Buffer.from(
+			JSON.stringify(paymentPayload)
+		).toString("base64");
+		const keyIndex = 1;
+		const string = payloadMain + "/pg/v1/pay" + MERCHANT_KEY;
+		const sha256 = crypto.createHash("sha256").update(string).digest("hex");
+		const checksum = sha256 + "###" + keyIndex;
+
+		const option = {
+			method: "POST",
+			url: MERCHANT_BASE_URL_PROD,
+			headers: {
+				accept: "application/json",
+				"Content-Type": "application/json",
+				"X-VERIFY": checksum,
+			},
+			data: {
+				request: payloadMain,
+			},
+		};
+
+		axios
+			.request(option)
+			.then((response) => {
+				console.log("ORD", response.data);
+				return res.redirect(
+					response.data.data.instrumentResponse.redirectInfo.url
+				);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+		// res.status(200).json({
+		// 	msg: "OK",
+		// 	url: response.data.data.instrumentResponse.redirectInfo.url,
+		// 	resp: response.data,
+		// });
 	} catch (error) {
 		console.log("error in payment", error);
 		res.status(500).json({ error: "Failed to initiate payment" });
@@ -85,23 +90,24 @@ exports.createOrder = async (req, res) => {
 };
 
 exports.veriFyPayment = async (req, res) => {
-	const merchantTransactionId = req.query.id;
 	console.log("STATUS", req.query);
+	const merchantTransactionId = req.req.body.transactionId;
+	const merchantId = res.req.body.merchantId;
 
 	const keyIndex = 1;
 	const string =
-		`/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}` + MERCHANT_KEY;
+		`/pg/v1/status/${merchantId}/${merchantTransactionId}` + MERCHANT_KEY;
 	const sha256 = crypto.createHash("sha256").update(string).digest("hex");
 	const checksum = sha256 + "###" + keyIndex;
 
 	const option = {
 		method: "GET",
-		url: `${MERCHANT_STATUS_URL_PROD}/${MERCHANT_ID}/${merchantTransactionId}`,
+		url: `${MERCHANT_STATUS_URL_PROD}/${merchantId}/${merchantTransactionId}`,
 		headers: {
 			accept: "application/json",
 			"Content-Type": "application/json",
 			"X-VERIFY": checksum,
-			"X-MERCHANT-ID": MERCHANT_ID,
+			"X-MERCHANT-ID": merchantId,
 		},
 	};
 
