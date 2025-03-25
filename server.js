247a4b68907e0e52714a30b376a0a314ff3a5447
ritshubh <shubhsharmass242424@gmail.com>
2025-03-26 01:29:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,20 +20,3 @@ app.listen(port, () => {
 	console.log(`Example listening at http://localhost:${port} `);
 });
 
-//priykala cred
-// const MERCHANT_KEY = "e238dc05-4252-419e-acf0-759b6242cb90";
-// const MERCHANT_ID = "M227WH8T7N8HO";
-
-// const MERCHANT_BASE_URL =
-// 	"https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
-// const MERCHANT_BASE_URL_PROD = "https://api.phonepe.com/apis/hermes/pg/v1/pay";
-
-// const MERCHANT_STATUS_URL =
-// 	"https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status";
-// const MERCHANT_STATUS_URL_PROD =
-// 	"https://api.phonepe.com/apis/hermes/pg/v1/status";
-
-// const redirectUrl = "http://localhost:4000/status";
-
-// const successUrl = "http://localhost:5173/payment-success";
-// const failureUrl = "http://localhost:5173/payment-failure";
